08d4751ad3c2f74579bbb5b6912945c0ac86ef8a
ethanleicht <ethanrleicht@gmail.com>
2023-09-24 21:07:50 -0400

    update sol1

--- solution_q1.py
+++ solution_q1.py
@@ -1,6 +1,6 @@
 import random
-from collections import deque
-
+import heapq
+import math
 
 
 def read(filepath):
@@ -14,107 +14,178 @@ def read(filepath):
 
 # Function to print the current state of the puzzle as a string
 def print_puzzle(state):
-    state_str = ",".join(map(str, state)).replace("0", "_")
+    state_str = ','.join(map(str, state)).replace('0', '_')
     print(state_str)
 
 
 
-def find_possible_moves(state, empty_index):
+def find_possible_moves(empty_index):
     possible_moves = []
 
-    # Define the possible moves (up, down, left, right)
-    if empty_index >= 3:
-        possible_moves.append(empty_index - 3)
-    if empty_index < 6:
-        possible_moves.append(empty_index + 3)
+    # Define the possible moves (left, right, up down)
     if empty_index % 3 != 0:
-        possible_moves.append(empty_index - 1)
+        possible_moves.append((empty_index - 1, 'R'))
+
     if empty_index % 3 != 2:
-        possible_moves.append(empty_index + 1)
-    
+        possible_moves.append((empty_index + 1, 'L'))
+
+    if empty_index >= 3:
+        possible_moves.append((empty_index - 3, 'D'))
+
+    if empty_index < 6:
+        possible_moves.append((empty_index + 3, 'U'))
+
     return possible_moves
 
 
 
 # Function to solve the 8-puzzle using random moves
 def dfs(initial, goal):
-    print("DFS path to solution:")
-
     state = initial
-    moves = 0
+    path = ''
 
     while state != goal:
-        print_puzzle(state)
-
         empty_index = state.index(0)
-        possible_moves = find_possible_moves(state, empty_index)
-        move = random.choice(possible_moves)
-        state[state.index(0)], state[move] = state[move], state[state.index(0)]
-        moves += 1
+        possible_moves = find_possible_moves(empty_index)
+        move, dir = random.choice(possible_moves)
 
-    # Print goal state
-    print_puzzle(state)
+        path.append(str(state[move]) + dir)
+        state[empty_index], state[move] = state[move], state[empty_index]
+
+    return path
 
 
 
 # Define the BFS function to solve the puzzle
 def bfs(initial, goal):
-    print("BFS path to solution:")
-
-    queue = deque([initial])
+    queue = [(initial, '')]
     visited = set()
+    node_expansions = 0
 
     while queue:
-        state = queue.popleft()
-        print_puzzle(state)
+        state, path = queue.pop(0)
         
         if state == goal:
+            print(path[:-1])
+            print('Node expansions:', node_expansions)
             break
 
         empty_index = state.index(0)
-        possible_moves = find_possible_moves(state, empty_index)
+        possible_moves = find_possible_moves(empty_index)
+        node_expansions += len(possible_moves)
 
-        for move in possible_moves:
-            new_state = state.copy()
+        visited.add(tuple(state))
+
+        for move, dir in possible_moves:
+            new_state = state[:]
             new_state[empty_index], new_state[move] = new_state[move], new_state[empty_index]
-
             if tuple(new_state) not in visited:
-                visited.add(tuple(new_state))
-                queue.append(new_state)
+                queue.append((new_state, path + str(state[move]) + dir + ','))
+   
 
 
-
+# Define the UCS function to solve the puzzle
 def ucs(initial, goal):
-    pass
+    visited = set()
+    queue = [(0, initial, '')]  # Priority queue with cost
+    node_expansions = 0  # Counter to track node expansions
+
+    while queue:
+        cost, state, path = heapq.heappop(queue)  # Get the state with the lowest cost
+
+        # Increment the node expansions count for each possible move
+        empty_index = state.index(0)
+        possible_moves = find_possible_moves(empty_index)
+        node_expansions += len(possible_moves)
+
+        if state == goal:
+            print(path[:-1])
+            print('Node expansions:', node_expansions)
+
+        visited.add(tuple(state))
+
+        for move, dir in possible_moves:
+            new_state = state[:]
+            new_state[empty_index], new_state[move] = new_state[move], new_state[empty_index]
+            if tuple(new_state) not in visited:
+                heapq.heappush(queue, (cost + 1, new_state, path + str(state[move]) + dir + ','))  # Add new state to the queue
 
 
 
-def a_star(initial, goal):
-    pass
+# Function to calculate the Manhattan distance heuristic
+def manhattan_distance(state):
+    total_distance = 0
+    for i in range(9):
+        if state[i] != 0:
+            target_row, target_col = divmod(state[i] - 1, 3)
+            current_row, current_col = divmod(i, 3)
+            total_distance += abs(target_row - current_row) + abs(target_col - current_col)
+    return total_distance
+
+def straight_line_distance(state):
+    total_distance = 0
+    for i in range(9):
+        if state[i] != 0:
+            target_row, target_col = divmod(state[i] - 1, 3)
+            current_row, current_col = divmod(i, 3)
+            distance = math.sqrt((target_row - current_row) ** 2 + (target_col - current_col) ** 2)
+            total_distance += distance
+    return total_distance
+
+def a_star(initial, goal, heuristic):
+    visited = set()
+    queue = [(0 + heuristic(initial), initial, '')]  # Priority queue with cost and heuristic
+    node_expansions = 0  # Counter to track node expansions
+
+    while queue:
+        cost, state, path = heapq.heappop(queue)  # Get the state with the lowest cost + heuristic
+
+        # Increment the node expansions count for each possible move
+        empty_index = state.index(0)
+        possible_moves = find_possible_moves(empty_index)
+        node_expansions += len(possible_moves)
+
+        if state == goal:
+            print(path[:-1])
+            print('Node expansions:', node_expansions)
+
+        visited.add(tuple(state))
+
+        for move, dir in possible_moves:
+            new_state = state[:]
+            new_state[empty_index], new_state[move] = new_state[move], new_state[empty_index]
+            if tuple(new_state) not in visited:
+                heapq.heappush(queue, (cost + 1 + heuristic(new_state), new_state, path + str(state[move]) + dir + ','))  # Add new state to the queue
 
 
 
-if __name__ == "__main__":
-    # Input initial state as a list (e.g., [1, 2, 3, 4, 0, 5, 7, 8, 6])
+if __name__ == '__main__':
+    # Input initial state as a list
     initial = read('input.txt')
+    print('Initial state:', initial)
+    print()
     
     # Define the goal state
     goal = [0, 1, 2, 3, 4, 5, 6, 7, 8]  # 0 represents the empty tile
-
-    print("The solution of Q1.1 is:")
-    print()
     
-    #dfs(initial, goal)
-    #print()
+    # print('The solution of Q1.1 (DFS) is:')
+    # dfs(initial, goal)
+    # print()
     
+    print('The solution of Q1.2 (BFS) is:')
     bfs(initial, goal)
     print()
 
+    print('The solution of Q1.2 (UCS) is:')
     ucs(initial, goal)
     print()
 
-    a_star(initial, goal)
+    print('The solution of Q1.2 (A* Manhattan Distance) is:')
+    a_star(initial, goal, manhattan_distance)
     print()
 
+    print('The solution of Q1.2 (A* Straight Line Distance) is:')
+    a_star(initial, goal, straight_line_distance)
+    print()
 
 
